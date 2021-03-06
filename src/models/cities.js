cc9d9d9c7c137a968f383b34e07ffb6268476f64
matita <matitald@gmail.com>
2021-03-06 23:31:59 +0000

    Deploying to gh-pages from @ matita/clocks@21cb4d19c4700c91aabe3172e8433f5053ff866a 🚀

--- src/models/cities.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  
-  search: function(text, callback) {
-    $.getJSON('http://gd.geobytes.com/AutoCompleteCity?callback=?', { q: text }, function(cities) {
-      callback(cities);
-    });
-  }
-
-};
--- src/utils/tmpl.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(tmplText) {
-  return function(obj) {
-    return $(tmplText.replace(/\{\{(\w+)\}\}/g, function(match, key) {
-      return obj[key] || '';
-    })).data('obj', obj);
-  };
-};
