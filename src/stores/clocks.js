8debbb1ec0115609a62e3721fa8222e54b397f9d
Matteo Colucci <matitald@gmail.com>
2021-03-08 00:17:29 +0100

    Fix adding duplicate clocks via link

--- src/stores/clocks.js
+++ src/stores/clocks.js
@@ -5,7 +5,22 @@ let id = 1;
 
 export default {
   subscribe,
-  add: (clock) => update((clocks) => clocks.concat({ ...clock, id: id++ })),
+  add: ({ location, minutesOffset, name }) => update((clocks) => {
+    if (clocks.some((clock) => (
+      clock.location === location
+        && clock.minutesOffset === minutesOffset
+        && clock.name === name
+    ))) {
+      return clocks;
+    }
+
+    return clocks.concat({
+      location,
+      minutesOffset,
+      name,
+      id: id++,
+    });
+  }),
   rename: (clock, newName) => update((clocks) => clocks.map((c) => {
     if (c !== clock) {
       return c;
