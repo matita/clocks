5962973682ccdf20a4063714ab0f443e3496d449
Matteo Colucci <matitald@gmail.com>
2021-03-07 14:28:11 +0100

    Improve style for actions

---
+++ src/components/Action.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let danger = false;
+</script>
+
+<button
+  class="uppercase px-2 focus:outline-none focus:ring-1 focus:ring-gray-300"
+  class:text-green-500={!danger}
+  class:text-red-500={danger}
+  on:click|stopPropagation
+>
+  <slot />
+</button>
--- src/components/SubClock.svelte
+++ src/components/SubClock.svelte
@@ -3,6 +3,7 @@
   let activeClock = writable(null);
 </script>
 <script>
+  import Action from './Action.svelte';
   import clocks from '../stores/clocks';
 
   export let clock;
@@ -39,16 +40,16 @@
   class:z-10={active}
   class:-translate-y-1={active}
   class:shadow-none={!active}
-  class:shadow-xl={active}
+  class:shadow-lg={active}
   on:click={onClick}
 >
   <div class="text-green-500">{clock.name || '---'}</div>
   <div class="text-xs text-gray-400">{clock.location}</div>
   {#if active}
     <button class="absolute top-1 right-1 px-2 focus:outline-none focus:ring-1" on:click|stopPropagation={() => active = false}>&times;</button>
-    <div class="clock-actions">
-      <button on:click|stopPropagation={onRenameClick}>Rename</button>
-      <button on:click|stopPropagation={onDeleteClick}>Delete</button>
+    <div class="text-right">
+      <Action danger on:click={onDeleteClick}>Delete</Action>
+      <Action on:click={onRenameClick}>Rename</Action>
     </div>
   {/if}
 </div>
